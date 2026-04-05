d22588806e4f2ed9931073a031de4724b0547e9b
darneyalzate <darney.alzate@gmail.com>
2026-04-05 00:17:42 -0500

    Delete views directory

--- views/vista_empleados.py
+++
@@ -1,8 +0,0 @@
-from tkinter import ttk
-
-class VistaEmpleados:
-    def __init__(self, notebook):
-        frame = ttk.Frame(notebook)
-        notebook.add(frame, text="👷 Empleados")
-
-        ttk.Label(frame, text="Vista de empleados funcionando").pack(pady=20)
--- views/vista_materiales.py
+++
@@ -1,8 +0,0 @@
-from tkinter import ttk
-
-class VistaMateriales:
-    def __init__(self, notebook):
-        frame = ttk.Frame(notebook)
-        notebook.add(frame, text="🧱 Materiales")
-
-        ttk.Label(frame, text="Vista de materiales funcionando").pack(pady=20)
